d273f4dea76b2599140b0254c80638b1e0f4d7b7
robert.wilde <robert.wilde@sponsoredlinx.com>
2018-11-06 16:25:05 +1000

    adding in the api consumer provider to test as well

--- web/app/Writer.php
+++ web/app/Writer.php
@@ -5,21 +5,18 @@ namespace App;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class Admin extends Authenticatable
+class Writer extends Authenticatable
 {
     use Notifiable;
 
-    protected $guard = 'admin';
+    protected $guard = 'writer';
 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password',
     ];
 
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token',
     ];
 
 }
